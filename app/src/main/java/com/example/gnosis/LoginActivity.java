698c2e77d52f898918814fed993d5b036c7aad5f
ADMIN <Sharlie.bkk@gmail.com>
2022-10-29 20:39:24 +1300

    Update

--- app/src/main/java/com/example/gnosis/LoginActivity.java
+++ app/src/main/java/com/example/gnosis/LoginActivity.java
@@ -2,8 +2,12 @@ package com.example.gnosis;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +19,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.concurrent.Executor;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -30,11 +36,69 @@ public class LoginActivity extends AppCompatActivity {
 
         currentUser = mAuth.getCurrentUser();
 
+        // creating a variable for our BiometricManager
+        // and lets check if our user can use biometric sensor or not
+        BiometricManager biometricManager = androidx.biometric.BiometricManager.from(this);
+        switch (biometricManager.canAuthenticate()) {
+
+            // this means we can use biometric sensor
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                //msgtex.setText("You can use the fingerprint sensor to login");
+                break;
+
+            // this means that the device doesn't have fingerprint sensor
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                //msgtex.setText("This device doesnot have a fingerprint sensor");
+                break;
+
+            // this means that biometric sensor is not available
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                //msgtex.setText("The biometric sensor is currently unavailable");
+                break;
+
+            // this means that the device doesn't contain your fingerprint
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                //msgtex.setText("Your device doesn't have fingerprint saved,please check your security settings");
+                break;
+        }
+
+        // creating a variable for our Executor
+        Executor executor = ContextCompat.getMainExecutor(this);
+        // this will give us result of AUTHENTICATION
+        final BiometricPrompt biometricPrompt = new BiometricPrompt(LoginActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+            }
+
+            // THIS METHOD IS CALLED WHEN AUTHENTICATION IS SUCCESS
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+                finishAffinity();
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+            }
+        });
+
+        // creating a variable for our promptInfo
+        // BIOMETRIC DIALOG
+        final BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder().setTitle("Gnosis")
+                .setDescription("Use your fingerprint to login ").setNegativeButtonText("Cancel").build();
+
+
+
         if(currentUser != null) {
-            Toast.makeText(getApplicationContext(), "Login successful!", Toast.LENGTH_LONG).show();
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            startActivity(intent);
-            return;
+            //Toast.makeText(getApplicationContext(), "Login successful!", Toast.LENGTH_LONG).show();
+            //Intent intent = new Intent(LoginActivity.this, BIOActivity.class);
+            //startActivity(intent);
+            biometricPrompt.authenticate(promptInfo);
+            //return;
         }
 
         TextView email = findViewById(R.id.etEmail);
